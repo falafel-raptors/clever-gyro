38f31b2b4d9e2dd27ac1951cdeac40aad6e56681
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-24 12:10:50 -0700

    about us scaffolded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// globals
+var gameState = {
+    rulesView: true,
+    victoryView: false,
+    targetValue: 0,
+    currentValue: 0,
+    cardUp: 0,
+    cardLeft: 0,
+    cardRight: 0,
+    cardDown: 0
+}
+
+//requirements:
+//function to generate target value
+//function to generate cards
+//generate first four numbers
+
+
+function init(){
+
+}
+
+init();

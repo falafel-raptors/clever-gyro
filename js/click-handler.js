1ba228c30054031f7c5ebf00f7711e8532536fed
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-25 15:47:09 -0700

    cleanup

--- js/click-handler.js
+++ js/click-handler.js
@@ -1,38 +1,34 @@
 'use strict';
 /*global gameState generateDeck storeInLocal countUpdater */
 
-// document.addEventListener('DOMContentLoaded', function(e) { 
-//   e.preventDefault();
-  var cards = document.getElementById('cards');
+var cards = document.getElementById('cards');
 
-
-  function handleClick (e) {
-    e.preventDefault();
-    //check the element based on that p, and increases current value
-    if(gameState.currentValue === gameState.targetValue){
-      cards.removeEventListener('click', handleClick);
-      // TODO: produce modal
-    } else {
-      if(e.target.parentElement.id === 'upButton'){
-        gameState.currentValue += countUpdater(gameState.cardUp);
-        buildNewCard('upButton');
-      }
-      if(e.target.parentElement.id === 'leftButton'){
-        gameState.currentValue += countUpdater(gameState.cardLeft);
-        buildNewCard('leftButton');
-      }
-      if(e.target.parentElement.id === 'rightButton'){
-        gameState.currentValue += countUpdater(gameState.cardRight);
-        buildNewCard('rightButton');
-      }
-      if(e.target.parentElement.id === 'downButton'){
-        gameState.currentValue += countUpdater(gameState.cardDown);
-        buildNewCard('downButton');
-      }
-    //save to local storage
-    storeInLocal ('gameState', gameState);
+function handleClick (e) {
+  e.preventDefault();
+  //check the element based on that p, and increases current value
+  if(gameState.currentValue === gameState.targetValue){
+    cards.removeEventListener('click', handleClick);
+    // TODO: produce modal
+  } else {
+    if(e.target.parentElement.id === 'upButton'){
+      gameState.currentValue += countUpdater(gameState.cardUp);
+      buildNewCard('upButton');
     }
+    else if(e.target.parentElement.id === 'leftButton'){
+      gameState.currentValue += countUpdater(gameState.cardLeft);
+      buildNewCard('leftButton');
+    }
+    else if(e.target.parentElement.id === 'rightButton'){
+      gameState.currentValue += countUpdater(gameState.cardRight);
+      buildNewCard('rightButton');
+    }
+    else if(e.target.parentElement.id === 'downButton'){
+      gameState.currentValue += countUpdater(gameState.cardDown);
+      buildNewCard('downButton');
+    }
+  //save to local storage
+  storeInLocal ('gameState', gameState);
   }
+}
 
-  cards.addEventListener('click', handleClick);
-// })
+cards.addEventListener('click', handleClick);

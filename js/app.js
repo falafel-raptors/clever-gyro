9eabd8aa89208dccf97d3a898814c629c3c2082e
trevorjdobson <trevorjdobson@gmail.com>
2019-06-27 10:01:30 -0700

    added hard reset and changed card defaults

--- js/app.js
+++ js/app.js
@@ -9,10 +9,26 @@ var gameState = {
   victoryView: false,
   targetValue: 0,
   currentValue: 0,
-  cardUp: 0,
-  cardLeft: 0,
-  cardRight: 0,
-  cardDown: 0,
+  cardUp: {
+    int: 0,
+    str: '0',
+    operator: '+'
+  },
+  cardLeft: {
+    int: 0,
+    str: '0',
+    operator: '+'
+  },
+  cardRight: {
+    int: 0,
+    str: '0',
+    operator: '+'
+  },
+  cardDown: {
+    int: 0,
+    str: '0',
+    operator: '+'
+  },
   minRange: 1,
   maxRange: 10,
   targetMin: 1,
@@ -51,6 +67,44 @@ function init(){
     generateScoreboard();
   }
   timer();
+}
+
+function hardReset(){
+  gameState = {
+    hasSeenRules: false,
+    rulesView: false,
+    victoryView: false,
+    targetValue: 0,
+    currentValue: 0,
+    cardUp: {
+      int: 0,
+      str: '0',
+      operator: '+'
+    },
+    cardLeft: {
+      int: 0,
+      str: '0',
+      operator: '+'
+    },
+    cardRight: {
+      int: 0,
+      str: '0',
+      operator: '+'
+    },
+    cardDown: {
+      int: 0,
+      str: '0',
+      operator: '+'
+    },
+    minRange: 1,
+    maxRange: 10,
+    targetMin: 1,
+    targetMax: 50,
+    timer: 0,
+  };
+  var leaderBoard = [];
+  var difficulty = 1;
+  localStorage.clear();
 }
 
 init();
